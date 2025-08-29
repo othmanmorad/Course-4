09ac8b4842434e5b69c97e82c87ccf7b71d28cc4
Othman Morad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-29 20:29:16 +0300

    Create problem13 course4.cpp

---
+++ problem13 course4.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+
+//by  refrence
+//void Readnumbers(int &num1 ,int &num2 ,int &num3 )
+//{   
+//    
+//    cout << "Enter number1: " << endl;
+//    cin >> num1;
+//
+//
+//    cout << "Enter number2: " << endl;
+//    cin >> num2;
+//
+//
+//    cout << "Enter number3: " << endl;
+//    cin >> num3;
+//
+//}
+//
+//int findMaxNumber(int num1, int num2, int num3)
+//{   
+//    int max;
+//    max = num1;
+//    if (num2 > max)
+//        max= num2;
+//     if (num3 > max)
+//        max=num3;
+//    return max;
+//}
+//
+//void PrintMaxNumber(int Max)
+//{
+//    cout << "The maximum number is : " << Max << endl;
+//}
+//int main()
+//{
+//    int num1, num2, num3;
+//    Readnumbers(num1, num2, num3);
+//    PrintMaxNumber(findMaxNumber(num1, num2, num3));
+//}  
+
+
+
+//by arrays 
+//void ReadNumbers(int arr[3])
+//{
+//	for (int i = 0;i < 3; i++)
+//	{
+//		cout << "Enter number " << i + 1 << endl;
+//		cin >> arr[i];
+//	}
+//}
+//int FindMaxNumber(int arr[3])
+//{
+//	int max = arr[0];
+//	if (arr[1] > max)
+//		max = arr[1];
+//	if (arr[2] > max)
+//		max = arr[2];
+//	return max;
+//}
+//void PrintMaxNumber(int max)
+//{
+//	cout << "Max number is :" << max;
+//}
+//int main()
+//{
+//	int arr[3];
+//
+//	ReadNumbers(arr);
+//	PrintMaxNumber(FindMaxNumber(arr));
+//}
+
+
